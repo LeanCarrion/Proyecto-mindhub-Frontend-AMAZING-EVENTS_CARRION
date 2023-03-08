ab40543b6f73f1d3c6bb302192ad0b0d538477fd
LeanCarrion <leandro.lescano1988@gmail.com>
2023-03-08 14:35:10 -0300

    hice el upcoming

---
+++ scripts/upcoming_events.js
@@ -0,0 +1,30 @@
+
+const contenedorCard = document.getElementById('card')
+  
+
+function mostrarCard (array,contenedor){
+    let tarjetas = ''
+    
+    
+for (dato of array) {
+    if(data.currentDate<dato.date){
+        tarjetas += `<div class="card m-3 mx-5" style="width: 14rem;">
+                    <img src="${dato.image}" class="card-img-top mt-2 h-100" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">${dato.name}</h5>
+                        <p class="card-text">${dato.description}</p>
+                    </div>
+
+                    <div class="card-body row d-flex">
+                        <p class="card-link d-flex col row">$${dato.price}</p>
+                        <button type="button" class="btn btn-primary card-link col row">See more</button>
+                    </div>
+                 </div>`
+
+    }
+}
+
+contenedor.innerHTML = tarjetas
+}
+
+mostrarCard(data.events,contenedorCard)
