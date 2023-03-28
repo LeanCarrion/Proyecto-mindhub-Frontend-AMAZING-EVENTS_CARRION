aec675bedccb83da732c047f445b178c71567a0d
LeanCarrion <leandro.lescano1988@gmail.com>
2023-03-27 22:54:09 -0300

    arranque la tabla

--- scripts/stats.js
+++ scripts/stats.js
@@ -1,16 +1,66 @@
-let NewEvento 
+let Evento 
 fetch('https://mindhub-xj03.onrender.com/api/amazing')
 .then((res)=>res.json())
 .then((datos)=>{
-  console.log(datos);
-  NewEvento = datos.events
-  console.log(NewEvento)
+//   console.log(datos);
+  Evento = datos.events
+//   console.log(Evento)
+
+
+
+  let otra = Evento.filter((elemento)=>(elemento.assistance))
+  console.log(otra)
+  let cave = otra.map((elemento) => (elemento.assistance * 100)/elemento.capacity);
+  console.log(cave)
+  let orden = cave.sort()
+  console.log(orden)
+  let masAsistencia = orden[0]
+  console.log(masAsistencia)
+  
+  
+
+  
+  
 })
-.catch((error)=>console.log(error))
-fetch('amazing.json')
-.then((res)=>res.json())
-.then((datos)=>{
-    console.log(datos);
-    datos = datos.events
-    console.log(datos)
-})
+// .catch((error)=>console.log(error))
+// fetch('amazing.json')
+// .then((res)=>res.json())
+// .then((datos)=>{
+//     console.log(datos);
+//     let Evento = datos.events
+//     console.log(Evento)
+    
+   
+// })
+
+// function porcentaje(objeto){
+//   per = (objeto.assistance * 100)/objeto.capacity
+//   return per    
+// }
+
+//  function arrayPorcentaje(array) {
+//     Aper = array.map(porcentaje())
+// }
+
+// events.map((event) => {
+//     event.percentageOfAssitance = (
+//       (event.assistance *100)/ event.capacity 
+//     ).toFixed(2);
+//     event.revenues = event.assistance * event.price;
+//   });
+
+
+
+  function crearcheckbox(arrayDeDatos) {
+    let checks = "";
+    let cave = Evento.map((elemento) => (elemento.assistance * 100)/elemento.capacity);
+    let catergorias = new Set(categoriasrepetidas);
+    catergorias.forEach((elemento) => {
+      checks += `<div class=" form-check form-check-inline ">
+                      <input class="form-check-input" type="checkbox" name="inlineRadioOptions" id="${elemento}"
+                      value="${elemento}">
+                      <label class="form-check-label" for="${elemento}">${elemento}</label>
+                  </div>`;
+    });
+    contenedorCheckbox.innerHTML = checks;
+  }
