079fe2aabf430d8754afa79e6ab9506cf68b735a
LeanCarrion <leandro.lescano1988@gmail.com>
2023-03-30 19:03:43 -0300

    push para fio

--- scripts/stats.js
+++ scripts/stats.js
@@ -1,9 +1,11 @@
 
 //Funcion
-let tEventStat = document.getElementById("firstTable");
+let primeraTabla = document.getElementById("firstTable");
+let segundaTabla =  document.querySelector('#secondTable');
+let terceraTabla =  document.querySelector('#thirdTable');
 
     function primeratabla(elemtento1, elemtento2,elemento3) {
-      tEventStat.innerHTML += `
+      primeraTabla.innerHTML += `
                 <tr>
                 <td>${elemtento1.name} - ${elemtento1.porcentage}%</td>
                 <td>${elemtento2.name} - ${elemtento2.porcentage}%</td>
@@ -11,6 +13,17 @@ let tEventStat = document.getElementById("firstTable");
                 
               </tr>
                 `;
+    }
+
+    function tablaUpcoming(item1) {
+      for (var i = 0; i < item1.length ; i++) {
+      segundaTabla.innerHTML += `
+                <tr>
+                <td>${item1[i].category}</td>
+                <td>revenue</td>
+                <td>percentaje</td>
+                `;
+      }
     }
 
 let Evento 
@@ -28,14 +41,13 @@ fetch('https://mindhub-xj03.onrender.com/api/amazing')
   // calculo y filtro de los porcentaje y ganancias
 
 let eventsUtiles = Evento.filter((elemento)=>(elemento.assistance))
-  console.log(eventsUtiles)
+  // console.log(eventsUtiles)
   eventsUtiles.map((elemento) => { elemento.porcentage = ((elemento.assistance * 100)/elemento.capacity).toFixed(2);})
   eventsUtiles.map((elemento) => { elemento.revenues = ((elemento.assistance * elemento.price).toFixed(2));})
   
   
 
-// console.log(Evento)
-console.log(eventsUtiles)
+// console.log(eventsUtiles)
 
 //ordenamiento de los datos segun capacidad
   
@@ -80,33 +92,87 @@ primeratabla(mostAftendances,lowestPercentage,mostCapacity);
 
 //tabla Upcoming events Statistics
 
+let eventosPasados = Evento.filter(eventoPast => eventoPast.date < fechaActual)
+let eventosFuturos =  Evento.filter(eventoUp => eventoUp.date > fechaActual);
 
-// Aca intente acumular por categoria
+console.log(eventosPasados)
+console.log(eventosFuturos)
 
-let EventsForCategory = [];
-    EventsForCategory.push(Evento.filter((event) => event.category === "Food"));
-    EventsForCategory.push(
-      Evento.filter((event) => event.category === "Cinema")
-    );
-    EventsForCategory.push(
-      Evento.filter((event) => event.category === "Party")
-    );
-    EventsForCategory.push(
-      Evento.filter((event) => event.category === "Books")
-    );
-    EventsForCategory.push(Evento.filter((event) => event.category === "Race"));
-    EventsForCategory.push(
-      Evento.filter((event) => event.category === "Concert")
-    );
-    EventsForCategory.push(
-      Evento.filter((event) => event.category === "Museum")
-    );
+// agrego revenues y assistences a eventos futuros
 
-    EventsForCategory = EventsForCategory.filter((array) => array.length !== 0);
-    console.log(EventsForCategory)
+eventosFuturos.map((ele) => { ele.porcentage = ((ele.estimate * 100)/ele.capacity).toFixed(2);})
+eventosFuturos.map((ele) => { ele.revenues = ((ele.estimate * ele.price).toFixed(2));})
+
+
+let categoriasEventosFuturos = [];
+let categoriasEventosPasados = [];
+
+function filtroCategorias(array, nuevoArray) {
+  array.forEach( evento => {
+    if (!nuevoArray.includes(evento.category)) {
+      nuevoArray.push(evento.category);
+    }
+  })
+  return nuevoArray;
+}
+filtroCategorias(eventosFuturos, categoriasEventosFuturos);
+filtroCategorias(eventosPasados, categoriasEventosPasados);
+
+console.log(categoriasEventosFuturos)
+console.log(categoriasEventosPasados)
+
+// encontrar los eventos que tengan las categorias filtradas.
+let eventosFuturosConCategoria = []
+let eventosPasadosConCategoria = []
+
+function eventosQueCompartenCategoria(array, arrayNuevo, tipoEvento) {
+  array.map( categoria => {
+    //arrayNuevo.push( tipoEvento.filter(evento => evento.category === categoria))
+    arrayNuevo.push({
+      category: categoria,
+      eventos: tipoEvento.filter(evento => evento.category === categoria)
+    })
+  })
+  return arrayNuevo;
+}
+eventosQueCompartenCategoria(categoriasEventosFuturos, eventosFuturosConCategoria, eventosFuturos);
+eventosQueCompartenCategoria(categoriasEventosPasados, eventosPasadosConCategoria, eventosPasados)
+
+console.log(eventosFuturosConCategoria);
+console.log(eventosPasadosConCategoria);
+
+// tablaUpcoming(eventosFuturosConCategoria)
+
+// GANANCIAS
+
+//UPCOMING GANANCIAS
+
+   
+let arrayFinal = [];
+
+for (i = 0 ; i < eventosFuturosConCategoria; i++) {
+  let categoryInicial = 0
+
+  for (evento of eventos){
+    let revenuesInicial = 0
+    let revenuesFinal = revenuesInicial + 1
 
   
+  
+}
+arrayFinal.push({
+  revenues: revenuesFinal
+})
+}
 
+
+
+
+
+console.log(arrayFinal)
+
+
+//
   
 })
 
